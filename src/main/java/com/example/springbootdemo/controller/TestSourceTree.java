e98b57c2e8295741483633827fa2c99f1f08794e
limx <2689430116@qq.com>
2019-10-21 12:38:53 +0800

    测试sourcetree

---
+++ src/main/java/com/example/springbootdemo/controller/TestSourceTree.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo.controller;
+
+/**
+ * @ProjectName: springbootdemo
+ * @Package: com.example.springbootdemo.controller
+ * @ClassName: TestSourceTree
+ * @Author: limingxing
+ * @Description: ${description}
+ * @Date: 2019/10/21 12:38
+ * @Version: 1.0
+ */
+public class TestSourceTree {
+}
